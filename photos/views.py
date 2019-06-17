34463034a7b626d4febc045a4ba103a982906683
ngishjonathan <balozimwash@gmil.com>
2019-06-17 14:23:28 +0300

    created views for our application

--- photos/views.py
+++ photos/views.py
@@ -3,4 +3,17 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my gallery')
+   return render(request,'welcome.html')
+   
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["search_item"]:
+        search_term = request.GET.get("search_item")
+        searched_images = Image.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photos/search.html',{"message":message})
