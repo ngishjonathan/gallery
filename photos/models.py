f52ee6977fdb5ee4d847c92e8af72e011a27faeb
ngishjonathan <balozimwash@gmil.com>
2019-06-17 14:33:58 +0300

    created a locatio category and image models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    image_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.image_location
+
+
+class Category(models.Model):
+    image_category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.image_category
+
+
+class Image(models.Model):
+    image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+    image_name = models.CharField(max_length=100)
+    image_description = models.TextField()
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
