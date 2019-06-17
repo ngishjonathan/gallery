7e74a9390a2df2537c86af7ccbe9ae81e132d71d
ngishjonathan <balozimwash@gmil.com>
2019-06-17 18:44:55 +0300

    added a location url for searching locations for images

--- photos/urls.py
+++ photos/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_results, name='search_results')
+urlpatterns = [
+    url('^$',views.index,name='index'),
+    url(r'^search/',views.search_images,name='search_results'),
+    url(r'^location/(\d+)',views.filter_location,name='located_images')
 ]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
