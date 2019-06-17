093b8d15c7005353445c7a17051c1fe7e4d41adf
ngishjonathan <balozimwash@gmil.com>
2019-06-17 18:46:07 +0300

    create a view to view specif images

--- photos/views.py
+++ photos/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from .models import Image,Location,Category
 
 # Create your views here.
-def welcome(request):
-   return render(request,'welcome.html')
-   
-def search_results(request):
+def index(request):
+    image_catalogue = Image.all_images()
+    location = Location.get_location()
+    return render(request,'all-photos/index.html',{"all_images":image_catalogue,"locations":location})
 
-    if 'image' in request.GET and request.GET["search_item"]:
-        search_term = request.GET.get("search_item")
-        searched_images = Image.search_by_title(search_term)
+def search_images(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        search_images = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+        return render(request,'all-photos/search.html',{"message":message,"images_searched":search_images})
 
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'all-photos/search.html',{"message":message})
+        message = "You haven't searched yet"
+        return render(request,"all-photos/search.html",{"message":message})
+
+def filter_location(request,image_id):
+    try:
+        location = Location.get_location()
+        located_images = Image.objects.filter(image_location=image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'all-photos/image.html',{"located_images":located_images,"locations":location})
